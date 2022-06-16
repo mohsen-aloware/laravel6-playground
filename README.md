6483b5af4ba2e38646188d5b5918e87c4e02208b
Mohsen Akhlaghi <mohsen@aloware.com>
2022-06-16 11:53:07 +0300

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # Laravel 6 Playground
+Simple dockerized Laravel 6 playground.
+
+## Terminal Commands
+Use `./dev.sh` helper utility to quickly run and work with your docker environment.
+
+#### Start all services
+```shell
+> ./dev.sh up
+```
+if you need to rebuild the images:
+```shell
+> ./dev.sh up --build
+```
+
+#### Stop all services
+```shell
+> ./dev.sh down
+```
+
+#### Service status
+```shell
+> ./dev.sh ps
+```
+
+#### Restart services
+```shell
+> ./dev.sh restart
+```
+To restart a specific service:
+```shell
+> ./dev.sh restart php
+> ./dev.sh restart nginx
+> ./dev.sh restart redis
+> ./dev.sh restart mysql
+```
+
+#### Access php bash
+```shell
+> ./dev.sh bash
+```
+
+#### Access other services bash
+```shell
+> ./dev.sh bash php
+> ./dev.sh bash nginx
+> ./dev.sh bash redis
+> ./dev.sh bash mysql
+```
+
+#### Execute something within php container
+```shell
+> ./dev.sh exec ...
+```
+example:
+```shell
+> ./dev.sh exec php artisan tinker
+> ./dev.sh exec php git status
+```
+
+#### Service(s) logs
+```shell
+> ./dev.sh logs
+> ./dev.sh logs php
+> ./dev.sh logs nginx
+> ./dev.sh logs mysql
+> ./dev.sh logs redis
+```
--- dev.sh
+++ dev.sh
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
 
+# Developed by Mohsen Akhlaghi [https://github.com/madagon]
+
+CUR_DIR=$(pwd)
+
 cd docker/dev
 
 if [[ "$1" == 'bash' ]]; then
-docker-compose -p playground exec php bash
+  if [[ "$2" != '' ]]; then
+    {
+      docker-compose -p l6_playground exec $2 bash
+    } || {
+      echo -e "\033[33m$2 doesn't support 'bash'. Trying 'sh' instead...\033[0m"
+      docker-compose -p l6_playground exec $2 sh
+    }
+  else
+    docker-compose -p l6_playground exec php bash
+  fi
 
 elif [[ "$1" == 'exec' ]]; then
-shift;
-docker-compose -p playground exec php $@
+  shift
+  docker-compose -p l6_playground exec php $@
 
 elif [[ "$1" == 'up' ]]; then
-shift;
-docker-compose -p playground up -d $@
+  shift
+  docker-compose -p l6_playground up -d $@
 
 elif [[ "$1" == 'down' ]]; then
-docker-compose -p playground down
+  docker-compose -p l6_playground down
 
 elif [[ "$1" == 'ps' ]]; then
-docker-compose -p playground ps
+  docker-compose -p l6_playground ps
 
 elif [[ "$1" == 'restart' ]]; then
-docker-compose -p playground restart
+  docker-compose -p l6_playground restart $2
 
 elif [[ "$1" == 'logs' ]]; then
-docker-compose -p playground logs -f --tail=100 php
+  docker-compose -p l6_playground logs -f --tail=100 $2
 
 fi
 
-cd ../../
+cd $CUR_DIR
